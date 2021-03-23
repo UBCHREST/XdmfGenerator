1ed24fb9a5d14018e1b1a449a43f5886c0c38c31
matt mcgurn <matt@mcgurn.dev>
2021-03-23 12:59:57 -0400

    removed petsc dep

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 static char help[] = "Command line executable to generate xdmf file hdf5 visualization. \n";
 
-#include <petsc.h>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -8,18 +7,12 @@ static char help[] = "Command line executable to generate xdmf file hdf5 visuali
 #include "xdmfBuilder.hpp"
 
 int main(int argc, char **args) {
-    // initialize petsc and mpi
-    PetscErrorCode err = PetscInitialize(&argc, &args, NULL, help);
-    CHKERRQ(err);
+    // Get the file path
 
-    // check to see if we should print options
-    char filename[PETSC_MAX_PATH_LEN] = "";
-    PetscBool fileSpecified = PETSC_FALSE;
-    PetscOptionsGetString(NULL, NULL, "--input", filename, PETSC_MAX_PATH_LEN, &fileSpecified);
-    CHKERRQ(err);
-    if (!fileSpecified) {
-        throw std::invalid_argument("the hdf5 file must be specified using the --input flag");
-    }
+
+//    if (!fileSpecified) {
+//        throw std::invalid_argument("the hdf5 file must be specified using the --input flag");
+//    }
 
     std::filesystem::path filePath(filename);
     if (!std::filesystem::exists(filePath)) {
@@ -43,6 +36,4 @@ int main(int argc, char **args) {
     xmlFile.close();
 
     std::cout << "XDMF file written to " << outputFile << std::endl;
-
-    return PetscFinalize();
 }
