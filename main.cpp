3e7295ff86daa772dfe97f6206dc632fce7d504f
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2021-03-24 07:45:54 -0400

    make clean (#4)
    * working on make
    
    * removed petsc dep
    
    * removed petsc dep
    
    * removed petsc dep
    
    * changed libs to private
    
    * static lib
    
    * debug code
    
    * debug code
    
    * debug code
    
    * debug code
    
    * simplifying make statement
    
    * cleaned up the code to produce simple interface and no other dep
    
    * made lib private
    
    * removed linking libs
    
    * formatting fix

--- main.cpp
+++ main.cpp
@@ -1,48 +1,26 @@
 static char help[] = "Command line executable to generate xdmf file hdf5 visualization. \n";
 
-#include <petsc.h>
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <memory>
-#include "xdmfBuilder.hpp"
+#include "generators.hpp"
 
 int main(int argc, char **args) {
-    // initialize petsc and mpi
-    PetscErrorCode err = PetscInitialize(&argc, &args, NULL, help);
-    CHKERRQ(err);
-
-    // check to see if we should print options
-    char filename[PETSC_MAX_PATH_LEN] = "";
-    PetscBool fileSpecified = PETSC_FALSE;
-    PetscOptionsGetString(NULL, NULL, "--input", filename, PETSC_MAX_PATH_LEN, &fileSpecified);
-    CHKERRQ(err);
-    if (!fileSpecified) {
-        throw std::invalid_argument("the hdf5 file must be specified using the --input flag");
+    // Get the file path
+    if (argc < 2) {
+        throw std::invalid_argument("the hdf5 file must be specified as the first argument");
     }
 
-    std::filesystem::path filePath(filename);
+    std::filesystem::path filePath(args[1]);
     if (!std::filesystem::exists(filePath)) {
         throw std::invalid_argument("unable to locate input file: " + filePath.string());
     }
-
-    // prepare
-    auto root = std::make_shared<petscXdmfGenerator::HdfObject>(filePath);
-    auto specification = petscXdmfGenerator::XdmfSpecification::FromPetscHdf(root);
-    auto builder = petscXdmfGenerator::XdmfBuilder(specification);
-    auto xml = builder.Build();
 
     // build the path to the output file
     std::filesystem::path outputFile = filePath.parent_path();
     outputFile /= (filePath.stem().string() + ".xmf");
 
     // write to the file
-    std::ofstream xmlFile;
-    xmlFile.open(outputFile);
-    xml->PrettyPrint(xmlFile);
-    xmlFile.close();
-
+    petscXdmfGenerator::Generate(filePath, outputFile);
     std::cout << "XDMF file written to " << outputFile << std::endl;
-
-    return PetscFinalize();
 }
