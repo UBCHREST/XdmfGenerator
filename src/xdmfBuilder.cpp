022c1212cba643d9de5af0f37ee92d8f8d16c3bc
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-09-01 12:55:02 -0400

    support for 1D lines in two spaces (#19)

--- src/xdmfBuilder.cpp
+++ src/xdmfBuilder.cpp
@@ -18,7 +18,7 @@ static std::map<unsigned long long, std::map<unsigned long long, std::string>> c
 /**
  * dim, corners to nodes per cell. Empty omits the NodesPerElement tag.  If negative set to the size of the system
  */
-static std::map<unsigned long long, std::map<unsigned long long, int>> nodesPerCell = {{1, {{0, -1}}}, {2, {{0, -1}, {2, 2}}}, {3, {{0, -1}}}};
+static std::map<unsigned long long, std::map<unsigned long long, int>> nodesPerCell = {{1, {{0, -1}, {2, 2}}}, {2, {{0, -1}, {2, 2}}}, {3, {{0, -1}}}};
 
 static std::map<FieldType, std::string> typeMap = {{SCALAR, "Scalar"}, {VECTOR, "Vector"}, {TENSOR, "Tensor6"}, {MATRIX, "Matrix"}};
 
